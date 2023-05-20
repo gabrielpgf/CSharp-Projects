9368196b5f66c0ee895352a422038da2fc2a6473
gabrielpgf <gabrielpgf2011@gmail.com>
2023-05-20 11:49:55 -0300

    Adding ApplicationUserModel and Views

--- Calendar/Data/ApplicationDbContext.cs
+++ Calendar/Data/ApplicationDbContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Calendar.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Calendar.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ Calendar/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Calendar.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
